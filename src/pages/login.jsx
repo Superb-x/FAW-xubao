adb0c5b0087471732e8648306b15e7990f25c657
liuxianglin <nearwind_x@163.com>
2017-09-14 22:09:27 +0800

    add some files

---
+++ src/pages/login.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>测试用</h1>
+            </div>
+        )
+    }
+}
