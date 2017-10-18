381657bc11ebca3f6a29a217f8b31aa1a64b3292
Superb-x <943572695@qq.com>
2017-10-18 11:24:49 +0800

    modified the axios interceptor

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,9 +1,13 @@
 import axios from 'axios'
 
-export default fetch = axios.create({
+const fetch = axios.create({
     baseURL: 'http://xubao.faw.chenghx.com',
     timeout: 10000,
     headers: { 
         'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' 
     },
+})
+
+fetch.interceptors.request(function(){
+    console.log("请求开始")
 })
